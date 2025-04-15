845b62c0c29bdd51b64388a971d577fb4dce1ba6
heyyouhere <max.kalan@gmail.com>
2025-04-15 05:14:14 +0300

    New character: next ws

--- character_controller.js
+++ character_controller.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as SkeletonUtils from 'three/addons/utils/SkeletonUtils.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -26,7 +27,7 @@ floor.rotation.x = Math.PI/2
 scene.add(floor)
 
 let gridHelper = new THREE.GridHelper( 40, 40 );
-scene.add( gridHelper );
+scene.add(gridHelper);
 
 
 function onProgress(p){
@@ -48,126 +49,153 @@ function loadModel(url) {
     });
 }
 
-
-
-
 let robot_glb = await loadModel('./models/walking.glb')
-let robot = robot_glb.scene
-scene.add(robot)
 
+function createTextSprite(message, color) {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    context.font = '24px Arial';
+    context.fillStyle = color;
+    const textWidth = context.measureText(message).width;
+    context.fillText(message, textWidth, 24);
 
-let mixer = new THREE.AnimationMixer(robot_glb.scene);
-let idle_clip = robot_glb.animations.find(clip => clip.name == 'Idle')
-let walk_clip = robot_glb.animations.find(clip => clip.name == 'Walk')
+    const texture = new THREE.Texture(canvas);
+    texture.needsUpdate = true;
 
-let idleAction = mixer.clipAction(idle_clip)
-let walkAction = mixer.clipAction(walk_clip)
+    const spriteMaterial = new THREE.SpriteMaterial({ map: texture });
+    const sprite = new THREE.Sprite(spriteMaterial);
+    sprite.scale.set(2, 1, 2);
 
-
-
-
-function playWalk(){
-    console.log('playing walk')
-    idleAction.fadeOut(0.1); // Fade out idle
-    walkAction.reset().fadeIn(0.1); // Reset and fade in walk
-    walkAction.play();
+    return sprite;
 }
 
-function playIdle(){
-    console.log('playing idle')
-    walkAction.fadeOut(0.1); // Fade out walk
-    idleAction.reset().fadeIn(0.1); // Reset and fade in idle
-    idleAction.play();
+
+
+class Player{
+    constructor(glb, text= 'test' ) {
+        this.scene = SkeletonUtils.clone(glb.scene); // https://discourse.threejs.org/t/how-to-clone-a-gltf/78858/4
+        this.mixer = new THREE.AnimationMixer(this.scene)
+        this.idleAction = this.mixer.clipAction(
+            robot_glb.animations.find(clip => clip.name == 'Idle')
+        )
+        this.walkAction = this.mixer.clipAction(
+            robot_glb.animations.find(clip => clip.name == 'Walk')
+        )
+        this.isWalking = false
+        this.speed = 0.03
+        this.lastTrack = this.lastTrack
+        this.currentTrack = 'Idle'
+        this.direction = new THREE.Vector3(0,0,1);
+        this.sprite = createTextSprite(text)
+        this.sprite.position.set(0, 1.5, 0)
+        this.scene.add(this.sprite)
+    }
+
+    playWalk(){
+        this.idleAction.fadeOut(0.1);
+        this.walkAction.reset().fadeIn(0.1);
+        this.walkAction.play();
+    }
+
+    playIdle(){
+        this.walkAction.fadeOut(0.1);
+        this.idleAction.reset().fadeIn(0.1);
+        this.idleAction.play();
+    }
+    handleMovement(keypressed){
+        temp = new THREE.Vector3().copy(this.scene.position)
+        temp.sub(camera.position)
+        temp.y = 0;
+        if (keypressed['w'] || keypressed['ц']){
+            temp.normalize();
+            this.direction = new THREE.Vector3().copy(this.direction).add(temp)
+            this.isWalking = true
+        }
+        if (keypressed['s'] || keypressed['ы']){
+            this.direction = new THREE.Vector3().copy(this.direction).add(temp.multiplyScalar(-1))
+            this.isWalking = true
+        }
+
+        if (keypressed['a'] || keypressed['ф']){
+            temp = new THREE.Vector3(-temp.z, 0, temp.x).multiplyScalar(-1)
+            if (keypressed['s']){
+                temp.multiplyScalar(-1)
+            }
+            this.direction = new THREE.Vector3().copy(this.direction).add(temp)
+            this.isWalking = true
+        }
+
+        if (keypressed['d'] || keypressed['в']){
+            temp = new THREE.Vector3(temp.z, 0, -temp.x).multiplyScalar(-1);
+            if (keypressed['s']){
+                temp.multiplyScalar(-1)
+            }
+            this.direction = new THREE.Vector3().copy(this.direction).add(temp)
+            this.isWalking = true
+        }
+
+        if (this.isWalking){
+            this.direction.normalize()
+            this.scene.position.add(this.direction.multiplyScalar(this.speed));
+            camera.position.x += this.direction.x
+            camera.position.z += this.direction.z
+            controls.target = new THREE.Vector3().copy(this.scene.position).add(new THREE.Vector3(0, 1, 0))
+            this.currentTrack = 'Walk'
+        } else {
+            this.currentTrack = 'Idle'
+        }
+        if (this.lastTrack !== this.currentTrack){
+            if (this.currentTrack == 'Idle'){
+                this.playIdle()
+            }
+            if (this.currentTrack == 'Walk'){
+                this.playWalk()
+            }
+            this.lastTrack = this.currentTrack
+        }
+
+        this.scene.lookAt(new THREE.Vector3(this.scene.position.x + this.direction.x, 0, this.scene.position.z + this.direction.z))
+    }
+
 }
 
+
+let player = new Player(robot_glb)
+scene.add(player.scene)
+
+let player2 = new Player(robot_glb)
+scene.add(player2.scene)
+player2.playIdle()
 
 
 
 const keypressed = {}
-
-
 window.onkeydown = (event) => {
     keypressed[event.key.toLowerCase()] = true
 }
 window.onkeyup = (event) => {
     keypressed[event.key.toLowerCase()] = false
-    isWalking = false
+    player.isWalking = false
 }
 
-let direction = new THREE.Vector3(0,0,1);
 let temp = new THREE.Vector3();
-let isWalking = false
-const moveDistance = 0.05;
 
-let currentTrack = 'Idle'
-let lastTrack = 'Idle'
-playIdle()
+player.playIdle()
 camera.position.y = 2
 camera.position.z = 4
-controls.target = new THREE.Vector3().copy(robot.position).add(new THREE.Vector3(0, 1, 0))
-function handleMovement(){
-    temp = new THREE.Vector3().copy(robot.position)
-    temp.sub(camera.position)
-    temp.y = 0;
-    if (keypressed['w'] || keypressed['ц']){
-        temp.normalize();
-        direction = new THREE.Vector3().copy(direction).add(temp)
-        isWalking = true
-    }
-    if (keypressed['s'] || keypressed['ы']){
-        direction = new THREE.Vector3().copy(direction).add(temp.multiplyScalar(-1))
-        isWalking = true
-    }
+controls.target = new THREE.Vector3().copy(player.scene.position).add(new THREE.Vector3(0, 1, 0))
 
-    if (keypressed['a'] || keypressed['ф']){
-        temp = new THREE.Vector3(-temp.z, 0, temp.x).multiplyScalar(-1)
-        if (keypressed['s']){
-            temp.multiplyScalar(-1)
-        }
-        direction = new THREE.Vector3().copy(direction).add(temp)
-        isWalking = true
-    }
-
-    if (keypressed['d'] || keypressed['в']){
-        temp = new THREE.Vector3(temp.z, 0, -temp.x).multiplyScalar(-1);
-        if (keypressed['s']){
-            temp.multiplyScalar(-1)
-        }
-        direction = new THREE.Vector3().copy(direction).add(temp)
-        isWalking = true
-    }
-
-    if (isWalking){
-        direction.normalize()
-        robot.position.add(direction.multiplyScalar(moveDistance));
-        camera.position.x += direction.x
-        camera.position.z += direction.z
-        controls.target = new THREE.Vector3().copy(robot.position).add(new THREE.Vector3(0, 1, 0))
-        currentTrack = 'Walk'
-    } else {
-        currentTrack = 'Idle'
-    }
-    if (lastTrack !== currentTrack){
-        if (currentTrack == 'Idle'){
-            playIdle()
-        }
-        if (currentTrack == 'Walk'){
-            playWalk()
-        }
-        lastTrack = currentTrack
-    }
-
-    robot.lookAt(new THREE.Vector3(robot.position.x + direction.x, 0, robot.position.z + direction.z))
-}
-
-
+let clock = new THREE.Clock();
 function animate() {
-    handleMovement()
-
+    document.getElementById("fps").innerText = Math.floor(1/clock.getDelta()) + 'fps'
+    player.handleMovement(keypressed)
     controls.update()
     renderer.render(scene, camera);
-    if (mixer) {
-        mixer.update(0.01);
+    if (player.mixer) {
+        player.mixer.update(0.01);
+    }
+    if (player2.mixer) {
+        player2.mixer.update(0.01);
     }
     requestAnimationFrame(animate);
 }
